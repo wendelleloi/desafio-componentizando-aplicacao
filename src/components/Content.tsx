401633d418d8d43d1740953e1f6735aa225ad6cb
wendelleloi <wendelleloi2@gmail.com>
2021-03-18 00:18:46 -0300

    complete content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,20 @@
-export function Content() {
-  // Complete aqui
+import { ContentProps, MovieProps } from './../interfaces';
+import { MovieCard } from './MovieCard';
+
+export const Content: React.FC<ContentProps> = (props) => {
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {props.genre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {props.movies.map((movie: MovieProps) => (
+              <MovieCard key={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
